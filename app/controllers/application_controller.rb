c023abefd6aafd11174cc3fd35dfb08a015bb231
Tanner Lancaster <saltillokid11@yahoo.com>
2016-03-04 22:44:28 -0500

    updated file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def heroku
-    render text: "hello Heroku!"
+    render text: "hello Heroku!!"
   end
 end
